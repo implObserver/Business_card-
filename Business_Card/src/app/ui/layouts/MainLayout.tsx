2b1e30d6de168285a42671c4c3a46dbb72226b1d
implObserver <prognozman@mail.ru>
2024-11-05 13:34:57 +0500

    style:change css of main layout

--- Business_Card/src/app/ui/layouts/MainLayout.tsx
+++ Business_Card/src/app/ui/layouts/MainLayout.tsx
@@ -6,33 +6,9 @@ import { Footer } from '@/widgets/footer';
 
 export const MainLayout: React.FC = () => {
     const { pathname } = useLocation();
-    const mainLayoutRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (mainLayoutRef.current) {
-                mainLayoutRef.current.style.backgroundPositionY = `${0}px`;
-            }
-        };
-
-        const handleResize = () => {
-            // Обновление при изменении высоты
-            if (mainLayoutRef.current) {
-                mainLayoutRef.current.style.backgroundPositionY = 'center';
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
     
     return (
-        <div ref={mainLayoutRef} className={styles.main_layout}>
+        <div className={styles.main_layout}>
             <div className={styles.plug}>
                 <header className={styles.header}>
                     <Header />
